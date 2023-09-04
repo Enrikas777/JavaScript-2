bdf836853805537de11b41853653e3573f7a2564
Enrikas777 <enrikas741@gmail.com>
2023-09-04 22:41:26 +0200

    JavaScript-2

---
+++ script.js
@@ -0,0 +1,6 @@
+var names = ["Jon Snow", "Cersei Lannister", "Daenerys Targaryen", "Theon Greyjoy", "Tyrion Lannister", "Arya Stark"]
+
+for(var i = 0; i < names.length; i++){
+        var count = i + 1
+        console.log(count + ". " + names[i])
+}
